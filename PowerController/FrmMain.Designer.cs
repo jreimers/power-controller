3de20a9d054adbec576e4d10e0c55fc4bdc1f217
John Reimers <johnanthony.reimers@gmail.com>
2016-02-28 00:01:59 -0500

    implement optimized data sampling and control outputs

---
+++ PowerController/FrmMain.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace PowerController
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotAmplitude = new OxyPlot.WindowsForms.PlotView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbComPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.gbForward = new System.Windows.Forms.GroupBox();
+            this.btnFwdCV = new System.Windows.Forms.CheckBox();
+            this.btnFwdCC = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFwdCurrent = new NumericTextBox();
+            this.tbFwdVoltage = new NumericTextBox();
+            this.btnRevCV = new System.Windows.Forms.CheckBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.gbReverse = new System.Windows.Forms.GroupBox();
+            this.btnRevCC = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbRevCurrent = new NumericTextBox();
+            this.tbRevVoltage = new NumericTextBox();
+            this.gbForward.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.gbReverse.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plotAmplitude
+            // 
+            this.plotAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotAmplitude.BackColor = System.Drawing.Color.White;
+            this.plotAmplitude.Location = new System.Drawing.Point(0, 39);
+            this.plotAmplitude.Name = "plotAmplitude";
+            this.plotAmplitude.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotAmplitude.Size = new System.Drawing.Size(872, 494);
+            this.plotAmplitude.TabIndex = 0;
+            this.plotAmplitude.Text = "plot1";
+            this.plotAmplitude.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotAmplitude.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotAmplitude.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM Port";
+            // 
+            // cbComPorts
+            // 
+            this.cbComPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbComPorts.FormattingEnabled = true;
+            this.cbComPorts.Location = new System.Drawing.Point(71, 12);
+            this.cbComPorts.Name = "cbComPorts";
+            this.cbComPorts.Size = new System.Drawing.Size(94, 21);
+            this.cbComPorts.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(171, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 21);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(252, 16);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 4;
+            // 
+            // gbForward
+            // 
+            this.gbForward.Controls.Add(this.btnFwdCV);
+            this.gbForward.Controls.Add(this.btnFwdCC);
+            this.gbForward.Controls.Add(this.label3);
+            this.gbForward.Controls.Add(this.label2);
+            this.gbForward.Controls.Add(this.tbFwdCurrent);
+            this.gbForward.Controls.Add(this.tbFwdVoltage);
+            this.gbForward.Location = new System.Drawing.Point(15, 547);
+            this.gbForward.Name = "gbForward";
+            this.gbForward.Size = new System.Drawing.Size(202, 79);
+            this.gbForward.TabIndex = 7;
+            this.gbForward.TabStop = false;
+            this.gbForward.Text = "Forward Mode";
+            // 
+            // btnFwdCV
+            // 
+            this.btnFwdCV.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnFwdCV.AutoSize = true;
+            this.btnFwdCV.FlatAppearance.BorderSize = 0;
+            this.btnFwdCV.Location = new System.Drawing.Point(94, 16);
+            this.btnFwdCV.Name = "btnFwdCV";
+            this.btnFwdCV.Size = new System.Drawing.Size(98, 23);
+            this.btnFwdCV.TabIndex = 10;
+            this.btnFwdCV.Tag = PowerController.FrmMain.ControllerMode.ForwardCV;
+            this.btnFwdCV.Text = "Constant Voltage";
+            this.btnFwdCV.UseVisualStyleBackColor = true;
+            this.btnFwdCV.CheckedChanged += new System.EventHandler(this.modeBtn_CheckedChanged);
+            // 
+            // btnFwdCC
+            // 
+            this.btnFwdCC.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnFwdCC.AutoSize = true;
+            this.btnFwdCC.Location = new System.Drawing.Point(94, 45);
+            this.btnFwdCC.Name = "btnFwdCC";
+            this.btnFwdCC.Size = new System.Drawing.Size(96, 23);
+            this.btnFwdCC.TabIndex = 14;
+            this.btnFwdCC.Tag = PowerController.FrmMain.ControllerMode.ForwardCC;
+            this.btnFwdCC.Text = "Constant Current";
+            this.btnFwdCC.UseVisualStyleBackColor = true;
+            this.btnFwdCC.CheckedChanged += new System.EventHandler(this.modeBtn_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "V";
+            // 
+            // tbFwdCurrent
+            // 
+            this.tbFwdCurrent.DecimalNumbers = 2;
+            this.tbFwdCurrent.ForeColor = System.Drawing.Color.Black;
+            this.tbFwdCurrent.Format = "^(\\-?)(\\d*)(\\.?)(\\d?)(\\d?)$";
+            this.tbFwdCurrent.HasNegatives = true;
+            this.tbFwdCurrent.Location = new System.Drawing.Point(6, 47);
+            this.tbFwdCurrent.Name = "tbFwdCurrent";
+            this.tbFwdCurrent.Size = new System.Drawing.Size(61, 20);
+            this.tbFwdCurrent.TabIndex = 9;
+            this.tbFwdCurrent.Tag = PowerController.FrmMain.ControllerMode.ForwardCC;
+
+            // 
+            // tbFwdVoltage
+            // 
+            this.tbFwdVoltage.DecimalNumbers = 2;
+            this.tbFwdVoltage.ForeColor = System.Drawing.Color.Black;
+            this.tbFwdVoltage.Format = "^(\\-?)(\\d*)(\\.?)(\\d?)(\\d?)$";
+            this.tbFwdVoltage.HasNegatives = true;
+            this.tbFwdVoltage.Location = new System.Drawing.Point(6, 18);
+            this.tbFwdVoltage.Name = "tbFwdVoltage";
+            this.tbFwdVoltage.Size = new System.Drawing.Size(61, 20);
+            this.tbFwdVoltage.TabIndex = 8;
+            this.tbFwdVoltage.Tag = PowerController.FrmMain.ControllerMode.ForwardCV;
+            // 
+            // btnRevCV
+            // 
+            this.btnRevCV.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnRevCV.AutoSize = true;
+            this.btnRevCV.Location = new System.Drawing.Point(94, 16);
+            this.btnRevCV.Name = "btnRevCV";
+            this.btnRevCV.Size = new System.Drawing.Size(98, 23);
+            this.btnRevCV.TabIndex = 11;
+            this.btnRevCV.Tag = PowerController.FrmMain.ControllerMode.ReverseCV;
+            this.btnRevCV.Text = "Constant Voltage";
+            this.btnRevCV.UseVisualStyleBackColor = true;
+            this.btnRevCV.CheckedChanged += new System.EventHandler(this.modeBtn_CheckedChanged);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 637);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(874, 22);
+            this.statusStrip.TabIndex = 8;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(61, 17);
+            this.lblStatus.Text = "Mode: Off";
+            // 
+            // gbReverse
+            // 
+            this.gbReverse.Controls.Add(this.btnRevCV);
+            this.gbReverse.Controls.Add(this.btnRevCC);
+            this.gbReverse.Controls.Add(this.label4);
+            this.gbReverse.Controls.Add(this.label5);
+            this.gbReverse.Controls.Add(this.tbRevCurrent);
+            this.gbReverse.Controls.Add(this.tbRevVoltage);
+            this.gbReverse.Location = new System.Drawing.Point(223, 547);
+            this.gbReverse.Name = "gbReverse";
+            this.gbReverse.Size = new System.Drawing.Size(202, 79);
+            this.gbReverse.TabIndex = 9;
+            this.gbReverse.TabStop = false;
+            this.gbReverse.Text = "Reverse Mode";
+            // 
+            // btnRevCC
+            // 
+            this.btnRevCC.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnRevCC.AutoSize = true;
+            this.btnRevCC.Location = new System.Drawing.Point(94, 45);
+            this.btnRevCC.Name = "btnRevCC";
+            this.btnRevCC.Size = new System.Drawing.Size(96, 23);
+            this.btnRevCC.TabIndex = 15;
+            this.btnRevCC.Tag = PowerController.FrmMain.ControllerMode.ReverseCC;
+            this.btnRevCC.Text = "Constant Current";
+            this.btnRevCC.UseVisualStyleBackColor = true;
+            this.btnRevCC.CheckedChanged += new System.EventHandler(this.modeBtn_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "V";
+            // 
+            // tbRevCurrent
+            // 
+            this.tbRevCurrent.DecimalNumbers = 2;
+            this.tbRevCurrent.ForeColor = System.Drawing.Color.Black;
+            this.tbRevCurrent.Format = "^(\\-?)(\\d*)(\\.?)(\\d?)(\\d?)$";
+            this.tbRevCurrent.HasNegatives = true;
+            this.tbRevCurrent.Location = new System.Drawing.Point(6, 47);
+            this.tbRevCurrent.Name = "tbRevCurrent";
+            this.tbRevCurrent.Tag = PowerController.FrmMain.ControllerMode.ReverseCC;
+            this.tbRevCurrent.Size = new System.Drawing.Size(61, 20);
+            this.tbRevCurrent.TabIndex = 9;
+            // 
+            // tbRevVoltage
+            // 
+            this.tbRevVoltage.DecimalNumbers = 2;
+            this.tbRevVoltage.ForeColor = System.Drawing.Color.Black;
+            this.tbRevVoltage.Format = "^(\\-?)(\\d*)(\\.?)(\\d?)(\\d?)$";
+            this.tbRevVoltage.HasNegatives = true;
+            this.tbRevVoltage.Location = new System.Drawing.Point(6, 18);
+            this.tbRevVoltage.Name = "tbRevVoltage";
+            this.tbRevVoltage.Tag = PowerController.FrmMain.ControllerMode.ReverseCV;
+            this.tbRevVoltage.Size = new System.Drawing.Size(61, 20);
+            this.tbRevVoltage.TabIndex = 8;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 659);
+            this.Controls.Add(this.gbReverse);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.gbForward);
+            this.Controls.Add(this.plotAmplitude);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbComPorts);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Power Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.gbForward.ResumeLayout(false);
+            this.gbForward.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.gbReverse.ResumeLayout(false);
+            this.gbReverse.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbComPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblError;
+        private OxyPlot.WindowsForms.PlotView plotAmplitude;
+        private System.Windows.Forms.GroupBox gbForward;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private NumericTextBox tbFwdCurrent;
+        private NumericTextBox tbFwdVoltage;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.GroupBox gbReverse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private NumericTextBox tbRevCurrent;
+        private NumericTextBox tbRevVoltage;
+        private System.Windows.Forms.CheckBox btnRevCV;
+        private System.Windows.Forms.CheckBox btnFwdCV;
+        private System.Windows.Forms.CheckBox btnRevCC;
+        private System.Windows.Forms.CheckBox btnFwdCC;
+    }
+}
---
+++ PowerController/FrmMain.cs
@@ -0,0 +1,285 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace PowerController
+{
+    public partial class FrmMain : Form
+    {
+        /// <summary>
+        /// Operating modes of the controller
+        /// </summary>
+        public enum ControllerMode
+        {
+            Off = 0,
+            ForwardCV = 1,
+            ForwardCC = 2,
+            ReverseCV = 3,
+            ReverseCC = 4
+        }
+        const byte PACKET_START = 0x53; // packet header
+        const byte PACKET_END = 0x45; // packet terminator
+        const byte FLOAT_SIZE = 4; // bytes in a float
+        const byte PAYLOAD_LENGTH = 4; // floats in a packet
+        const byte PAYLOAD_BYTES = FLOAT_SIZE*PAYLOAD_LENGTH; // bytes in a payload
+        const byte PACKET_LENGTH = PAYLOAD_BYTES + 2; // bytes in packet plus 2 bytes for start/end
+
+        /// <summary>
+        /// Packet structure to unpack raw bytes into float array
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = PAYLOAD_BYTES)]
+        struct Packet
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = PAYLOAD_LENGTH, ArraySubType = UnmanagedType.R4)]
+            public float[] data;
+        }
+
+        const int PLOT_BUFFER = 128; // number of data points to plot
+
+        /// <summary>
+        /// List of serial port names
+        /// </summary>
+        public String[] SerialPortsList
+        {
+            get;
+            private set;
+        }
+        public bool Running
+        {
+            get;
+            set;
+        }
+        public SerialPort Port
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Currently active controller mode
+        /// </summary>
+        public ControllerMode Mode
+        {
+            get;
+            set;
+        }
+
+        // line series for data plot
+        public LineSeries InputVoltage { get; set; }
+        public LineSeries InputCurrent { get; set; }
+        public LineSeries OutputVoltage { get; set; }
+        public LineSeries OutputCurrent { get; set; }
+
+        private CheckBox[] modeSelectors; // mode selector buttons
+        private NumericTextBox[] modeValues; // mode numeric inputs
+
+        /// <summary>
+        /// Oxyplot model for data plotting
+        /// </summary>
+        public PlotModel Model
+        {
+            get;
+            set;
+        }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            this.Mode = ControllerMode.Off;
+            modeSelectors = new CheckBox[] { btnFwdCC, btnFwdCV, btnRevCC, btnRevCV };
+            modeValues = new NumericTextBox[] { tbFwdCurrent, tbFwdVoltage, tbRevVoltage, tbRevCurrent };
+
+            InputVoltage = new LineSeries() { Title = "Input Voltage" };
+            InputCurrent = new LineSeries() { Title = "Input Current" };
+            OutputVoltage = new LineSeries() { Title = "Output Voltage" };
+            OutputCurrent = new LineSeries() { Title = "Output Current" };
+            Model = new PlotModel { };
+            Model.Series.Add(InputVoltage);
+            Model.Series.Add(InputCurrent);
+            Model.Series.Add(OutputVoltage);
+            Model.Series.Add(OutputCurrent);
+            this.plotAmplitude.Model = Model;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            this.SerialPortsList = SerialPort.GetPortNames();
+            cbComPorts.DataSource = this.SerialPortsList;
+            this.Running = true;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (Port != null && Port.IsOpen)
+            {
+                Port.Close();
+                btnConnect.Text = "Connect";
+                return;
+            }
+            if (cbComPorts.SelectedItem == null || String.IsNullOrEmpty((string)cbComPorts.SelectedItem))
+            {
+                return;
+            }
+            string comPort = (string)cbComPorts.SelectedItem;
+
+            Port = new SerialPort(comPort, 115200);
+
+            try
+            {
+                Port.Open();
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+                return;
+            }
+
+            Thread t = new Thread(new ParameterizedThreadStart(
+                delegate (object serialPort)
+                {
+                    SerialPort sp = (SerialPort)serialPort;
+                    while (this.Running)
+                    {
+                        try
+                        {
+                            List<Packet> data = new List<Packet>();
+                            while (sp.BytesToRead > PACKET_LENGTH)
+                            {
+                                int input = sp.ReadByte();
+                                if (input != PACKET_START)
+                                {
+                                    continue;
+                                }
+
+                                byte[] packetBytes = new byte[PAYLOAD_BYTES];
+                                sp.Read(packetBytes, 0, PAYLOAD_BYTES);
+
+                                GCHandle handle = GCHandle.Alloc(packetBytes, GCHandleType.Pinned);
+                                Packet p = Marshal.PtrToStructure<Packet>(handle.AddrOfPinnedObject());
+                                    
+                                input = sp.ReadByte();
+                                if(input == PACKET_END)
+                                {
+                                    data.Add(p);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Packet Read Error: {0:X}", input);
+                                }
+                            }
+                            if (data.Count > 0)
+                            {
+                                Invoke(new EventHandler(ProcessData), data, EventArgs.Empty);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            sp.Close();
+                            return;
+                        }
+                    }
+                }
+            ));
+            t.Start(Port);
+
+            this.btnConnect.Text = "Disconnect";
+
+            lblError.Text = "";
+        }
+        int x = 0;
+        private void ProcessData(object sender, EventArgs e)
+        {
+            List<Packet> dataPoints = (List<Packet>)sender;
+            foreach(Packet p in dataPoints)
+            {
+                x++;
+                addToMovingWindow(InputVoltage, x, p.data[0]);
+                addToMovingWindow(InputCurrent, x, p.data[1]);
+                addToMovingWindow(OutputVoltage, x, p.data[2]);
+                addToMovingWindow(OutputCurrent, x, p.data[3]);
+            }
+            Model.InvalidatePlot(true);
+        }
+        private void addToMovingWindow(LineSeries data, int x, float y)
+        {
+            if(data.Points.Count > PLOT_BUFFER)
+            {
+                data.Points.RemoveAt(0);
+            }
+            data.Points.Add(new DataPoint((double)x, (double)y));
+        }
+
+        private bool controllerTransmit()
+        {
+            try
+            {
+                float mode = (float)this.Mode;
+                float value = 0;
+                foreach (var tb in modeValues)
+                {
+                    if ((ControllerMode)tb.Tag == this.Mode)
+                    {
+                        value = tb.getFloatValue();
+                    }
+                }
+                this.Port.Write(new byte[] { PACKET_START }, 0, 1);
+                this.Port.Write(BitConverter.GetBytes(mode), 0, FLOAT_SIZE);
+                this.Port.Write(BitConverter.GetBytes(value), 0, FLOAT_SIZE);
+                this.Port.Write(new byte[] { PACKET_END }, 0, 1);
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Running = false;
+        }
+
+        private void OnControllerModeChange(ControllerMode newMode)
+        {
+            lblStatus.Text = "Mode: " + newMode.ToString();
+            this.Mode = newMode;
+            if(!controllerTransmit())
+            {
+                foreach (CheckBox cb in modeSelectors)
+                {
+                        cb.Checked = false;
+                }
+                return;
+            }
+            foreach (CheckBox cb in modeSelectors)
+            {
+                if((ControllerMode)cb.Tag != newMode)
+                {
+                    cb.Checked = false;
+                }
+            }
+        }
+
+        private void modeBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox btn = (CheckBox)sender;
+            ControllerMode newMode = (ControllerMode)btn.Tag;
+            gbForward.Focus();
+            if(this.Mode == newMode && btn.Checked == false)
+            {
+                OnControllerModeChange(ControllerMode.Off);
+            }
+            else if (btn.Checked)
+            {
+                OnControllerModeChange(newMode);
+            }
+        }
+    }
+}
